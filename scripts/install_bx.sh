08a14416b3d38fe4e85d158e01052796bc55d427
alex <irazabal@us.ibm.com>
2017-10-13 12:35:38 -0400

    added install_bx.sh

--- scripts/install_bx.sh
+++ scripts/install_bx.sh
@@ -23,3 +23,16 @@ if [ -n "$DEBUG" ]; then
   bx --version
   bx plugin list
 fi
+IP_ADDR=$(bx cs workers $CLUSTER_NAME | grep normal | awk '{ print $2 }')
+if [ -z $IP_ADDR ]; then
+  echo "$CLUSTER_NAME not created or workers not ready"
+  exit 1
+fi
+
+echo -e "Configuring vars"
+exp=$(bx cs cluster-config $CLUSTER_NAME | grep export)
+if [ $? -ne 0 ]; then
+  echo "Cluster $CLUSTER_NAME not created or not ready."
+  exit 1
+fi
+eval "$exp"
